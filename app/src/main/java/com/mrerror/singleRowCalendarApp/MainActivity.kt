72855107a4551f6e1d9174c710df5e9306553b7f
unknown <Ahmed.nader1994@gmail.com>
2023-01-09 23:08:51 +0200

    Add screenshots

--- app/src/main/java/com/mrerror/singleRowCalendarApp/MainActivity.kt
+++ app/src/main/java/com/mrerror/singleRowCalendarApp/MainActivity.kt
@@ -4,16 +4,24 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.mrerror.singleRowCalendar.SingleRowCalendar
 import com.mrerror.singleRowCalendarApp.ui.theme.SingleRowCalendarTheme
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,12 +33,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val context = LocalContext.current
-                    SingleRowCalendar(modifier = Modifier, onSelectedDayChange = {
-                        Toast.makeText(context, "${it.time}", Toast.LENGTH_SHORT).show()
-                    })
+                    MyApp()
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp() {
+    Column(Modifier.fillMaxSize()) {
+        var day by remember { mutableStateOf(Date()) }
+
+        SingleRowCalendar(onSelectedDayChange = {
+            day = it
+        })
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.Center),
+                text = "Selected Day is ${
+                    SimpleDateFormat(
+                        "dd-MM-yyyy",
+                        Locale.US
+                    ).format(day)
+                }"
+            )
         }
     }
 }
